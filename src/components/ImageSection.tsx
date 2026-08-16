c5f6433356e663a010a055395b459cb1d5a7d65d
yaya23-pro <yd91157@gmail.com>
2026-08-16 19:01:02 +0100

    Improve footer mobile responsiveness

--- src/components/ImageSection.tsx
+++ src/components/ImageSection.tsx
@@ -7,14 +7,14 @@ type ImageSectionProps = {
 export default function ImageSection({ src, alt, title }: ImageSectionProps) {
   return (
     <section className="relative w-full flex flex-col items-center bg-boza-cream-alt">
-      <div className="w-full h-[130vh] flex justify-center items-center max-[968px]:h-[85vh] max-[640px]:h-[55vh]">
+      <div className="w-full h-[90vh] flex justify-center items-center max-[968px]:h-[65vh] max-[640px]:h-[45vh]">
         <img
           src={src}
           alt={alt}
-          className="w-[60%] h-full object-contain max-[968px]:w-[80%] max-[640px]:w-[92%]"
+          className="w-full h-full object-cover"
         />
       </div>
-      <h1 className="w-[60%] text-right pr-0 pt-2.5 pb-6 font-display text-base font-black tracking-wide uppercase text-boza-black max-[968px]:w-[80%] max-[640px]:w-[92%] max-[640px]:text-sm max-[640px]:pb-8">
+      <h1 className="w-full text-right pr-4 pt-2.5 pb-6 font-display text-base font-black tracking-wide uppercase text-boza-black max-[640px]:text-sm max-[640px]:pb-8">
         {title}
       </h1>
     </section>
